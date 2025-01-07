15a0f03262eb4b9c4a01c0f9e536c5837eaf39ad
italiancode <italianloader@gmail.com>
2025-01-07 18:55:21 +0100

    Something just got updated!

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import "./index.css";
 
 import { AuthProvider } from "./contexts/AuthContext";
 import AppIndex from ".";
+import { isExtension } from "./utils/environment";
 
 const App: React.FC = () => {
   // For web app, use routing
   return (
-    <div className="app-container mx-auto w-[400px] h-[600px]">
+    <div className={`app-container mx-auto w-[400px] ${isExtension() ? 'h-[600px]' : 'min-h-screen'}`}>
       <AuthProvider>
         <main className="h-full">
           <AppIndex />
